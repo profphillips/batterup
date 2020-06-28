927f5a694227620be5bd480a96cd9ad01f96674d
John Phillips <profphillips@gmail.com>
2020-06-28 11:49:19 -0500

    removed leading slash in images

--- src/app.js
+++ src/app.js
@@ -5,20 +5,20 @@
 // Batting Order App by John Phillips on 6/29/2020
 
 const teamArray = [
-  { name: 'Ann', order: 1, image: '/images/girl1.jpg' },
-  { name: 'Barb', order: 2, image: '/images/girl2.jpg' },
-  { name: 'Carie', order: 3, image: '/images/girl3.jpg' },
-  { name: 'Diana', order: 4, image: '/images/girl4.jpg' },
-  { name: 'Elizabeth', order: 5, image: '/images/girl5.jpg' },
-  { name: 'Frankie', order: 6, image: '/images/girl6.jpg' },
-  { name: 'Gracie', order: 7, image: '/images/girl7.jpg' },
-  { name: 'Happy', order: 8, image: '/images/girl8.jpg' },
-  { name: 'Itty', order: 9, image: '/images/girl9.jpg' },
-  { name: 'Johna', order: 10, image: '/images/girl10.jpg' },
-  { name: 'Kate', order: 11, image: '/images/girl11.jpg' },
-  { name: 'Lefty', order: 12, image: '/images/girl12.jpg' },
-  { name: 'Chloe', order: 13, image: '/images/chloe.png' },
-  { name: 'Haley', order: 14, image: '/images/haley.png' },
+  { name: 'Ann', order: 1, image: 'images/girl1.jpg' },
+  { name: 'Barb', order: 2, image: 'images/girl2.jpg' },
+  { name: 'Carie', order: 3, image: 'images/girl3.jpg' },
+  { name: 'Diana', order: 4, image: 'images/girl4.jpg' },
+  { name: 'Elizabeth', order: 5, image: 'images/girl5.jpg' },
+  { name: 'Frankie', order: 6, image: 'images/girl6.jpg' },
+  { name: 'Gracie', order: 7, image: 'images/girl7.jpg' },
+  { name: 'Happy', order: 8, image: 'images/girl8.jpg' },
+  { name: 'Itty', order: 9, image: 'images/girl9.jpg' },
+  { name: 'Johna', order: 10, image: 'images/girl10.jpg' },
+  { name: 'Kate', order: 11, image: 'images/girl11.jpg' },
+  { name: 'Lefty', order: 12, image: 'images/girl12.jpg' },
+  { name: 'Chloe', order: 13, image: 'images/chloe.png' },
+  { name: 'Haley', order: 14, image: 'images/haley.png' },
 ];
 
 const app = {
@@ -67,19 +67,19 @@ const render = () => {
       <h2>Coaches</h2>
       <div className="footer">
         <div className="card">
-          <img src="/images/tessa.jpg" />
+          <img src="images/tessa.jpg" />
           <div className="cardContainer wrapper2">Coach Tessa</div>
         </div>
         <div className="card">
-          <img src="/images/larry.png" />
+          <img src="images/larry.png" />
           <div className="cardContainer wrapper2">Coach Larry</div>
         </div>
         <div className="card">
-          <img src="/images/moe.png" />
+          <img src="images/moe.png" />
           <div className="cardContainer wrapper2">Coach Moe</div>
         </div>
         <div className="card">
-          <img src="/images/curly.png" />
+          <img src="images/curly.png" />
           <div className="cardContainer wrapper2">Coach Curly</div>
         </div>
       </div>
